126469365ed7fd40d9d3cd794ce953a15bea4157
madeha0 <madehamalik01@gmail.com>
2025-01-26 13:26:49 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -157,5 +157,5 @@ def stop_camera():
     return jsonify({"status": "Camera stopped"})
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
