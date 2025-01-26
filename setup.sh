931b25418a26ac2b48af1bd4385e3dbace0b4194
madeha0 <madehamalik01@gmail.com>
2025-01-26 12:22:46 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
-# setup.sh
-#!/bin/bash
-apt-get update && apt-get install -y libopenblas-dev
+#!/usr/bin/env bash
+apt-get update && apt-get install -y \
+    libopenblas-dev \
+    liblapack-dev \
+    libx11-dev \
+    && apt-get clean
